57005a6cc56e0286726ff34109ee20536707e321
Kevin Benjamin <108537389+Kevin-benjamin@users.noreply.github.com>
2025-06-26 12:51:41 +0100

    Update

--- lib/permit.ts
+++ lib/permit.ts
@@ -4,5 +4,5 @@ export const permit = new Permit({
   //  Our Docker instance is running on port 7766
   pdp: "http://localhost:7766",
   // Add your api token also to a .env file with the variable below
-  token: process.env.NEXT_PUBLIC_PERMIT_API_KEY,
+  token: process.env.PERMIT_API_KEY,
 })
